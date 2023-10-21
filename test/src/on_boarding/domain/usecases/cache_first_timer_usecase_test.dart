8eb2d86550920dd26d65fdaceb385040ca43ab95
Emilio Nicoletti <eamnicoletti@gmail.com>
2023-10-21 19:28:23 -0300

    :test_tube: test: creates the cache_first_timer_usecase_test

---
+++ test/src/on_boarding/domain/usecases/cache_first_timer_usecase_test.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:edu_app/core/errors/failures.dart';
+import 'package:edu_app/src/on_boarding/domain/repos/on_boarding_repository.dart';
+import 'package:edu_app/src/on_boarding/domain/usecases/cache_first_timer_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'on_boarding_repo.mock.dart';
+
+void main() {
+  late OnBoardingRepo repo;
+  late CacheFirstTimerUsecase usecase;
+
+  setUp(() {
+    repo = MockOnBoardingRepo();
+    usecase = CacheFirstTimerUsecase(repo);
+  });
+
+  test(
+    'should call the [OnBoardingRepo.cacheFirstTimer] '
+    'and return the right data',
+    () async {
+      when(() => repo.cacheFirstTimer()).thenAnswer(
+        (_) async => Left(
+          ServerFailure(message: 'Unknown Error Occurred', statusCode: 500),
+        ),
+      );
+
+      final result = await usecase();
+
+      expect(
+        result,
+        equals(
+          Left<Failure, dynamic>(
+            ServerFailure(message: 'Unknown Error Occurred', statusCode: 500),
+          ),
+        ),
+      );
+
+      verify(() => repo.cacheFirstTimer()).called(1);
+      verifyNoMoreInteractions(repo);
+    },
+  );
+}
