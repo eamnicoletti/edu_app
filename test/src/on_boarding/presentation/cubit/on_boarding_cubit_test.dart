a408bb69d70c893af14dcd433e0504c4d359c516
Emilio Nicoletti <eamnicoletti@gmail.com>
2023-10-28 18:20:44 -0300

    :white_check_mark: test: adds the cacheFirstTimer group test

--- test/src/on_boarding/presentation/cubit/on_boarding_cubit_test.dart
+++ test/src/on_boarding/presentation/cubit/on_boarding_cubit_test.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:edu_app/src/on_boarding/domain/usecases/cache_first_timer_usecase.dart';
 import 'package:edu_app/src/on_boarding/domain/usecases/check_if_user_is_first_timer_usecase.dart';
 import 'package:edu_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
@@ -10,20 +12,41 @@ class MockCheckIfUserIsFirstTimer extends Mock
     implements CheckIfUserIsFirstTimerUsecase {}
 
 void main() {
-  late CacheFirstTimerUsecase cacheFirstTimer;
+  late CacheFirstTimerUsecase cacheFirstTimerUseCase;
   late CheckIfUserIsFirstTimerUsecase checkIfUserIsFirstTimer;
-  late OnBoardingCubit onBoardingCubit;
+  late OnBoardingCubit cubit;
 
   setUp(() {
-    cacheFirstTimer = MockCacheFirstTimer();
+    cacheFirstTimerUseCase = MockCacheFirstTimer();
     checkIfUserIsFirstTimer = MockCheckIfUserIsFirstTimer();
-    onBoardingCubit = OnBoardingCubit(
-      cacheFirstTimer: cacheFirstTimer,
+    cubit = OnBoardingCubit(
+      cacheFirstTimer: cacheFirstTimerUseCase,
       checkIfUserIsFirstTimer: checkIfUserIsFirstTimer,
     );
   });
 
   test('initial state should be [OnBoardingInitial]', () {
-    expect(onBoardingCubit.state, const OnBoardingInitial());
+    expect(cubit.state, const OnBoardingInitial());
+  });
+
+  group('cacheFirstTimer', () {
+    blocTest<OnBoardingCubit, OnBoardingState>(
+      'should emit [CachingFirstTImer, USerCached] when successful',
+      build: () {
+        when(() => cacheFirstTimerUseCase()).thenAnswer(
+          (_) async => const Right(null),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.cacheFirstTimer(),
+      expect: () => const [
+        CachingFirstTimer(),
+        UserCached(),
+      ],
+      verify: (_) {
+        verify(() => cacheFirstTimerUseCase()).called(1);
+        verifyNoMoreInteractions(cacheFirstTimerUseCase);
+      },
+    );
   });
 }
