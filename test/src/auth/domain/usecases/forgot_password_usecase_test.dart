81e1f7f5cdf7a8275b19c1fddd38972e47c53b1e
Emilio Nicoletti <eamnicoletti@gmail.com>
2023-10-29 20:30:07 -0300

    :test_tube: test: creates the forgot_password_usecase test

--- test/src/auth/domain/usecases/forgot_password_usecase_test.dart
+++ test/src/auth/domain/usecases/forgot_password_usecase_test.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:edu_app/src/auth/domain/entities/user_entity.dart';
-import 'package:edu_app/src/auth/domain/usecases/sign_in_usecase.dart';
+import 'package:edu_app/src/auth/domain/usecases/forgot_password_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -8,38 +7,28 @@ import 'auth_repo.mock.dart';
 
 void main() {
   late MockAuthRepo repo;
-  late SignInUsecase usecase;
+  late ForgotPasswordUsecase usecase;
 
   const tEmail = 'Test email';
-  const tPassword = 'Test password';
 
   setUp(() {
     repo = MockAuthRepo();
-    usecase = SignInUsecase(repo);
+    usecase = ForgotPasswordUsecase(repo);
   });
 
-  const tUser = UserEntity.empty();
-
-  test('should return [UserEntity] from the [AuthRepo]', () async {
+  test('should complete successfuly when call the repo is successful',
+      () async {
     // arrange
-    when(
-      () => repo.signIn(
-        email: any(named: 'email'),
-        password: any(named: 'password'),
-      ),
-    ).thenAnswer((_) async => const Right(tUser));
-
-    // act
-    final result = await usecase(
-      const SignInParams(
-        email: tEmail,
-        password: tPassword,
-      ),
+    when(() => repo.forgotPassword(any())).thenAnswer(
+      (_) async => const Right(null),
     );
 
+    // act
+    final result = await usecase(tEmail);
+
     // assert
-    expect(result, const Right<dynamic, UserEntity>(tUser));
-    verify(() => repo.signIn(email: tEmail, password: tPassword)).called(1);
+    expect(result, equals(const Right<dynamic, void>(null)));
+    verify(() => repo.forgotPassword(tEmail)).called(1);
     verifyNoMoreInteractions(repo);
   });
 }
