7f309f528534b6677853fd4860852919d41565a4
Emilio Nicoletti <eamnicoletti@gmail.com>
2024-02-13 16:42:15 -0300

    feat: creates the auth usecases

---
+++ lib/src/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,25 @@
+import 'package:edu_app/core/enums/update_user.dart';
+import 'package:edu_app/core/utils/typedfs.dart';
+import 'package:edu_app/src/auth/domain/entities/local_user_entity.dart';
+
+abstract class AuthRepo {
+  const AuthRepo();
+
+  ResultFuture<void> forgotPassword(String email);
+
+  ResultFuture<LocalUserEntity> signIn({
+    required String email,
+    required String password,
+  });
+
+  ResultFuture<LocalUserEntity> signUp({
+    required String email,
+    required String fullName,
+    required String password,
+  });
+
+  ResultFuture<void> updateUser({
+    required UpdateUserAction action,
+    dynamic userData,
+  });
+}
