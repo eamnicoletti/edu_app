764c61f7322b9620815c70f47533ce4ec926c773
Emilio Nicoletti <eamnicoletti@gmail.com>
2024-02-13 16:45:38 -0300

    feat: creates the auth user_model

---
+++ lib/src/auth/data/models/user_model.dart
@@ -0,0 +1,81 @@
+import 'package:edu_app/core/utils/typedfs.dart';
+import 'package:edu_app/src/auth/domain/entities/local_user_entity.dart';
+
+class LocalUserModel extends LocalUserEntity {
+  const LocalUserModel({
+    required super.uid,
+    required super.email,
+    required super.points,
+    required super.fullName,
+    super.groupId,
+    super.enrolledCourseIds,
+    super.following,
+    super.followers,
+    super.profilePic,
+    super.bio,
+  });
+
+  const LocalUserModel.empty()
+      : this(
+          uid: '',
+          email: '',
+          points: 0,
+          fullName: '',
+        );
+
+  LocalUserModel.fromMap(DataMap map)
+      : super(
+          uid: map['uid'] as String,
+          email: map['email'] as String,
+          points: (map['points'] as num).toInt(),
+          fullName: map['fullName'] as String,
+          profilePic: map['profilePic'] as String?,
+          bio: map['bio'] as String?,
+          groupId: (map['groupId'] as List<dynamic>).cast<String>(),
+          enrolledCourseIds:
+              (map['enrolledCourseIds'] as List<dynamic>).cast<String>(),
+          following: (map['following'] as List<dynamic>).cast<String>(),
+          followers: (map['followers'] as List<dynamic>).cast<String>(),
+        );
+
+  LocalUserEntity copyWith({
+    String? uid,
+    String? email,
+    String? profilePic,
+    String? bio,
+    int? points,
+    String? fullName,
+    List<String>? groupId,
+    List<String>? enrolledCourseIds,
+    List<String>? following,
+    List<String>? followers,
+  }) {
+    return LocalUserEntity(
+      uid: uid ?? this.uid,
+      email: email ?? this.email,
+      profilePic: profilePic ?? this.profilePic,
+      bio: bio ?? this.bio,
+      points: points ?? this.points,
+      fullName: fullName ?? this.fullName,
+      groupId: groupId ?? this.groupId,
+      enrolledCourseIds: enrolledCourseIds ?? this.enrolledCourseIds,
+      following: following ?? this.following,
+      followers: followers ?? this.followers,
+    );
+  }
+
+  DataMap toMap() {
+    return <String, dynamic>{
+      'uid': uid,
+      'email': email,
+      'profilePic': profilePic,
+      'bio': bio,
+      'points': points,
+      'fullName': fullName,
+      'groupId': groupId,
+      'enrolledCourseIds': enrolledCourseIds,
+      'following': following,
+      'followers': followers,
+    };
+  }
+}
