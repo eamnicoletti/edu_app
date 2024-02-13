08e9d57e7aed4ed643d0dbed1b369d656d8edbad
Emilio Nicoletti <eamnicoletti@gmail.com>
2024-02-13 16:41:07 -0300

    feat: creates the auth usecases

---
+++ lib/src/auth/domain/usecases/forgot_password_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:edu_app/core/usecases/usecases.dart';
+import 'package:edu_app/core/utils/typedfs.dart';
+import 'package:edu_app/src/auth/domain/repos/auth_repo.dart';
+
+class ForgotPasswordUsecase extends UsecaseWithParams<void, String> {
+  const ForgotPasswordUsecase(this._repo);
+
+  final AuthRepo _repo;
+
+  @override
+  ResultFuture<void> call(String params) => _repo.forgotPassword(params);
+}
---
+++ lib/src/auth/domain/usecases/sign_in_usecase.dart
@@ -0,0 +1,34 @@
+import 'package:edu_app/core/usecases/usecases.dart';
+import 'package:edu_app/core/utils/typedfs.dart';
+import 'package:edu_app/src/auth/domain/entities/local_user_entity.dart';
+import 'package:edu_app/src/auth/domain/repos/auth_repo.dart';
+import 'package:equatable/equatable.dart';
+
+class SignInUsecase extends UsecaseWithParams<LocalUserEntity, SignInParams> {
+  const SignInUsecase(this._repo);
+
+  final AuthRepo _repo;
+
+  @override
+  ResultFuture<LocalUserEntity> call(SignInParams params) => _repo.signIn(
+        email: params.email,
+        password: params.password,
+      );
+}
+
+class SignInParams extends Equatable {
+  const SignInParams({
+    required this.email,
+    required this.password,
+  });
+
+  const SignInParams.empty()
+      : email = '',
+        password = '';
+
+  final String email;
+  final String password;
+
+  @override
+  List<Object?> get props => [email, password];
+}
