4005e17c93c07417ea0391b2b52fb59c997a0b09
Emilio Nicoletti <eamnicoletti@gmail.com>
2024-06-03 19:55:31 -0300

    :test_tube: test: creates tests for AuthBloc class

--- test/src/auth/presentation/bloc/auth_bloc_test.dart
+++ test/src/auth/presentation/bloc/auth_bloc_test.dart
@@ -1,3 +1,7 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:edu_app/core/errors/failures.dart';
+import 'package:edu_app/src/auth/domain/entities/local_user_entity.dart';
 import 'package:edu_app/src/auth/domain/usecases/forgot_password_usecase.dart';
 import 'package:edu_app/src/auth/domain/usecases/sign_in_usecase.dart';
 import 'package:edu_app/src/auth/domain/usecases/sign_up_usecase.dart';
@@ -48,5 +52,60 @@ void main() {
 
   test('initialState should be [AuthInitialState]', () {
     expect(authBloc.state, const AuthInitialState());
+  });
+
+  final tServerFailure = ServerFailure(
+    message: 'user-not-found',
+    statusCode: 'There is no user record corresponding to this identifier. '
+        'The user may have been deleted',
+  );
+
+  group('SignInEvent', () {
+    const tUser = LocalUserEntity.empty();
+
+    blocTest<AuthBloc, AuthState>(
+      'should emit [AuthLoadingState, SignInState] '
+      'when signIn succeeds',
+      build: () {
+        when(() => signIn(any())).thenAnswer((_) async => const Right(tUser));
+        return authBloc;
+      },
+      act: (bloc) => bloc.add(
+        SignInEvent(
+          email: tSignInParams.email,
+          password: tSignInParams.password,
+        ),
+      ),
+      expect: () => [
+        const AuthLoadingState(),
+        const SignedInState(tUser),
+      ],
+      verify: (_) {
+        verify(() => signIn(tSignInParams)).called(1);
+        verifyNoMoreInteractions(signIn);
+      },
+    );
+
+    blocTest<AuthBloc, AuthState>(
+      'should emit [authLoading, authError] when signIn fails',
+      build: () {
+        when(() => signIn(any())).thenAnswer((_) async => Left(tServerFailure));
+        return authBloc;
+      },
+      act: (bloc) => bloc.add(
+        SignInEvent(
+          email: tSignInParams.email,
+          password: tSignInParams.password,
+        ),
+      ),
+      expect: () => [
+        const AuthLoadingState(),
+        AuthErrorState(tServerFailure.errorMessage),
+      ],
+      verify: (_) {
+        verify(() => signIn(tSignInParams)).called(1);
+        verifyNoMoreInteractions(signIn);
+      },
+    );
   });
 }
