df418e78ea0d691227409e0e674d2d16c8999b67
Emilio Nicoletti <eamnicoletti@gmail.com>
2024-06-04 16:15:13 -0300

    :sparkles: feat: creates the user_provider to manage user globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:edu_app/core/common/app/providers/user_provider.dart';
 import 'package:edu_app/core/res/colours.dart';
 import 'package:edu_app/core/res/fonts.dart';
 import 'package:edu_app/core/services/injection_container.dart';
@@ -5,6 +6,7 @@ import 'package:edu_app/core/services/router.dart';
 import 'package:edu_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,17 +23,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Edu',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: Fonts.poppins,
-        appBarTheme: const AppBarTheme(color: Colors.transparent),
-        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        title: 'Edu',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(color: Colors.transparent),
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: Colours.primaryColour,
+          ),
+        ),
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
